2c8394f896f27c12cbc0556ce6788382aa84c49e
dax <dax26d@gmail.com>
2023-07-04 21:24:17 +0800

    modified install commands

--- install.sh
+++ install.sh
@@ -9,11 +9,6 @@ if [[ ! -f ~/.zshrc ]]; then
 	echo "created symbol link for .zshrc"
 fi
 
-if [[ ! -f ~/.config/nvim/init.vim ]]; then
-	ln -s ~/dotfiles/init.vim ~/.config/nvim
-	echo "created symbol link for nvim init.vim"
-fi
-
 if [[ ! -f ~/.config/zathura/zathurarc ]]; then
 	ln -s ~/dotfiles/zathurarc ~/.config/zathura
 	echo "created symbol link for zathurarc"
@@ -27,20 +22,4 @@ fi
 if [[ ! -f ~/.latexmkrc ]]; then
 	ln -s ~/dotfiles/.latexmkrc ~
 	echo "created symbol link for .latexmkrc"
-fi
-
-if [[ ! -f ~/.local/share/nvim/plugged/vim-easymotion/t/all.snippets ]]; then
-	ln -s ~/dotfiles/all.snippets ~/.local/share/nvim/plugged/vim-easymotion/t
-	echo "created symbol link for ultisnip all.snippets"
-fi
-
-
-if [[ ! -f ~/.local/share/nvim/plugged/vim-easymotion/t/tex.snippets ]]; then
-	ln -s ~/dotfiles/tex.snippets ~/.local/share/nvim/plugged/vim-easymotion/t
-	echo "created symbol link for ultisnip tex.snippets"
-fi
-
-if [[ ! -f ~/.local/share/nvim/plugged/vim-easymotion/t/sinppets.snippets ]]; then
-	ln -s ~/dotfiles/snippets.snippets ~/.local/share/nvim/plugged/vim-easymotion/t
-	echo "created symbol link for ultisnip sinppets.snippets"
 fi
