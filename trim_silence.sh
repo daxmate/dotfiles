de0efbaa578492f824b7684aab7b1c5e15b85da5
dax <dax26d@gmail.com>
2023-12-29 11:28:35 +0800

    change name from remove_silence to trim_silence

--- trim_silence.sh
+++ trim_silence.sh
@@ -2,7 +2,7 @@ if [ -z "$VIRTUAL_ENV" ]; then
 	source ~/codes/video_tools/.venv/bin/activate
 fi
 
-~/Codes/video_tools/remove_silence.py $1
+~/Codes/video_tools/trim_silence.py $1
 
 if [ -n "$VIRTUAL_ENV" ]; then
 	deactivate
