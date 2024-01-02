e78494bd9615802ab0dd85b95c907caddce5bcbd
dax <dax26d@gmail.com>
2024-01-02 15:04:14 +0800

    add install function and install list

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 # create symbol links for configuration files
+
 if [[ ! -f ~/.vimrc ]]; then
 	ln -s ~/dotfiles/.vimrc ~
 	echo "created symbol link for .vimrc"
@@ -7,11 +8,6 @@ fi
 if [[ ! -f ~/.zshrc ]]; then
 	ln -s ~/dotfiles/.zshrc ~
 	echo "created symbol link for .zshrc"
-fi
-
-if [[ ! -f ~/.config/zathura/zathurarc ]]; then
-	ln -s ~/dotfiles/zathurarc ~/.config/zathura
-	echo "created symbol link for zathurarc"
 fi
 
 if [[ ! -f ~/.gitconfig ]]; then
@@ -36,4 +32,57 @@ if [[ ! -d ~/.oh-my-zsh/custom/plugins/myhistory ]]; then
 	echo "created symbol link for myhistory plugin"
 fi
 
+function install(){
+	cmd=$1
+	if type $cmd > /dev/null 2>&1; then
+		echo "$cmd already existed"
+	else
+		brew install $cmd
+	fi
+}
 
+# formulae
+install rg
+install eza
+install autojump
+install fd
+install ffmpeg
+install fzf
+install figlet
+install gcc
+install git
+install llvm
+install lua
+install luarocks
+install python
+install thefuck
+install tree
+install yt-dlp
+install neovim
+install zoxide
+install zsh-autopair
+install zsh-autosuggestions
+install zsh-syntax-highlighting
+install zsh-vi-mode
+install zsh-you-should-use
+
+# casks
+install alt-tab
+install mactex
+install sioyek
+install anki
+install macvim
+install squirrel
+install calibre
+install mks
+install tex-live-utility
+install google-chrome
+install mpv
+install wechat
+install iina
+install obs
+install wpsoffice-cn
+install karabiner-elements
+install pycharm-ce
+install keycastr
+install rectangle
