29425d645d5fb8032b569b192408fee87f3e51ca
dax <dax26d@gmail.com>
2024-01-07 10:34:11 +0800

    delete history line no. and sort^unique

--- myhistory/myhistory.plugin.zsh
+++ myhistory/myhistory.plugin.zsh
@@ -1,4 +1,4 @@
 alias h='history'
 alias hl='history | nvim'
-alias hs='history | rg'
-alias hsi='history | rg -i'
+alias hs="history | awk '{\$1=\"\"; sub(/^ /, \"\"); print \$0}' | sort | uniq | rg"
+alias hsi="history | awk '{\$1=\"\"; sub(/^ /, \"\"); print \$0}' | sort | uniq | rg -i"
