8ea584072f28a223b401edaa6e4095d2906b2200
dax <dax26d@gmail.com>
2026-01-29 00:35:42 +0800

    修改install代码

--- install.sh
+++ install.sh
@@ -2,147 +2,140 @@
 
 # create symbol links for configuration files
 
-if [[ ! -f ~/.vimrc ]]; then
-	ln -s ~/dotfiles/.vimrc ~
-	echo "created symbol link for .vimrc"
-fi
-
-if [[ ! -f ~/.zshrc ]]; then
-	ln -s ~/dotfiles/.zshrc ~
-	echo "created symbol link for .zshrc"
-fi
-
-if [[ ! -f ~/.gitconfig ]]; then
-	ln -s ~/dotfiles/.gitconfig ~
-	echo "created symbol link for .gitconfig"
-fi
-
-if [[ ! -f ~/.latexmkrc ]]; then
-	ln -s ~/dotfiles/.latexmkrc ~
-	echo "created symbol link for .latexmkrc"
-fi
-
-if [[ ! -f ~/.local/share/nvim/site/pack/packer/start/neoformat/autoload/neoformat/formatters/cpp.vim ]]; then
-	ln -s ~/dotfiles/cpp-neoformatter.vim ~/.local/share/nvim/site/pack/packer/start/neoformat/autoload/neoformat/formatters/cpp.vim
-	echo "created symbol link for cpp neoformatter"
-else
-	ln -fs ~/dotfiles/cpp-neoformatter.vim ~/.local/share/nvim/site/pack/packer/start/neoformat/autoload/neoformat/formatters/cpp.vim
-	echo "cpp neoformatter existed, force linking to the one in dotfiles folder"
-fi
-
-if [[ ! -f ~/.oh-my-zsh/custom/themes/agnoster-customized.zsh-theme ]]; then
-	ln -s ~/dotfiles/agnoster-customized.zsh-theme ~/.oh-my-zsh/custom/themes/agnoster-customized.zsh-theme
-	echo "created symbol link for agnoster-customized theme"
-fi
-
-
-if [[ ! -d ~/.oh-my-zsh/custom/plugins/myhistory ]]; then
-	ln -s ~/dotfiles/myhistory ~/.oh-my-zsh/custom/plugins/myhistory
-	echo "created symbol link for myhistory plugin"
-fi
-
-if [[ ! -d ~/.oh-my-zsh/custom/plugins/mytimer ]]; then
-	ln -s ~/dotfiles/mytimer ~/.oh-my-zsh/custom/plugins/mytimer
-	echo "created symbol link for mytimer plugin"
-fi
-
-if [[ ! -d ~/.oh-my-zsh/custom/plugins/mydirhistory ]]; then
-	ln -s ~/dotfiles/mydirhistory ~/.oh-my-zsh/custom/plugins/mydirhistory
-	echo "created symbol link for mydirhistory plugin"
-fi
-
-ln -fs ~/dotfiles/pac.js ~/Library/Application\ Support/V2RayX/pac/pac.js
-
-ln -fs ~/dotfiles/tex.vim ~/.local/share/nvim/site/pack/packer/start/neoformat/autoload/neoformat/formatters/tex.vim
-ln -fs ~/dotfiles/.mylatexindent.yaml ~/.mylatexindent.yaml
-ln -fs ~/dotfiles/.indentconfig.yaml ~/.indentconfig.yaml
-
-#sioyek
-ln -fs ~/dotfiles/sioyek/keys_user.config ~/Library/Application\ Support/sioyek/keys_user.config
-ln -fs ~/dotfiles/sioyek/prefs_user.config ~/Library/Application\ Support/sioyek/prefs_user.config
-
-installed_formulae=(`brew list --formulae`)
-installed_casks=(`brew list --casks`)
-function install(){
-	cmd=$2
-	if [[ "$1" == formula ]]; then
-		if [[ $cmd == "python" ]] && [[ ${installed_formulae[(I)$cmd]} ]]; then
-			echo "Python already installed, skip"
-		elif [[ ${installed_formulae[(Ie)$cmd]} ]]; then
-			echo "$cmd already installed, skip"
-		else
-			brew install "$cmd"
-		fi
-	else
-		if [[ ${installed_casks[(Ie)$cmd]} ]]; then
-			echo "$cmd already installed, skip"
-		else
-			brew install --cask --force $cmd
-		fi
-	fi
+# 定义链接创建函数
+# 参数: mode src dst
+# mode: "skip" - 如果目标存在则跳过；"force" - 强制覆盖
+create_link() {
+    local mode=$1
+    local src=$2
+    local dst=$3
+    
+    case $mode in
+        skip)
+            if [[ -e $dst ]]; then
+                echo "$dst already exists, skip"
+                return
+            fi
+            ln -s "$src" "$dst"
+            echo "created symbol link for $dst"
+            ;;
+        force)
+            if [[ -e $dst ]]; then
+                echo "$dst existed, force linking to $src"
+            else
+                echo "created symbol link for $dst"
+            fi
+            ln -sf "$src" "$dst"
+            ;;
+    esac
 }
 
+# 使用数组存储链接配置：每行包含 mode src dst
+links=(
+    # 如果目标不存在则创建的链接
+    "skip" "$HOME/dotfiles/.vimrc" "$HOME/.vimrc"
+    "skip" "$HOME/dotfiles/.zshrc" "$HOME/.zshrc"
+    "skip" "$HOME/dotfiles/.gitconfig" "$HOME/.gitconfig"
+    "skip" "$HOME/dotfiles/.latexmkrc" "$HOME/.latexmkrc"
+    # oh-my-zsh 主题和插件
+    "skip" "$HOME/dotfiles/agnoster-customized.zsh-theme" "$HOME/.oh-my-zsh/custom/themes/agnoster-customized.zsh-theme"
+    # 强制覆盖的链接
+    "force" "$HOME/dotfiles/.mylatexindent.yaml" "$HOME/.mylatexindent.yaml"
+    "force" "$HOME/dotfiles/.indentconfig.yaml" "$HOME/.indentconfig.yaml"
+    # sioyek 配置
+    "force" "$HOME/dotfiles/sioyek/keys_user.config" "$HOME/Library/Application Support/sioyek/keys_user.config"
+    "force" "$HOME/dotfiles/sioyek/prefs_user.config" "$HOME/Library/Application Support/sioyek/prefs_user.config"
+)
+
+
+# 批量创建符号链接
+for mode src dst in $links; do
+	create_link $mode "$src" "$dst"
+done
+
+
+installed_formulae=($(brew list --formulae))
+installed_casks=($(brew list --casks))
+
+function install(){
+    local type=$1
+    local cmd=$2
+    
+    if [[ $type == formula ]]; then
+        # 使用 -gt 0 检查精确匹配
+        if [[ ${installed_formulae[(Ie)$cmd]} -gt 0 ]]; then
+            echo "$cmd already installed, skip"
+        else
+            brew install "$cmd"
+        fi
+    else
+        if [[ ${installed_casks[(Ie)$cmd]} -gt 0 ]]; then
+            echo "$cmd already installed, skip"
+        else
+            brew install --cask --force $cmd
+        fi
+    fi
+}
 
 # formulae
 formulae=(
-bat
-eza
-fd
-ffmpeg
-figlet
-fortune
-fzf
-gcc
-git
-llvm
-lolcat
-lua
-luarocks
-neovim
-nnn
-python
-ripgrep
-thefuck
-tldr
-vifm
-yt-dlp
-zoxide
+	awk
+	black
+	cmake
+	curl
+	dust
+	gh
+	imagemagick
+	jq
+	qt
+	rename
+	tmux
+    bat
+    eza
+    fd
+    ffmpeg
+    figlet
+    fortune
+    fzf
+    gcc
+    git
+    llvm
+    lolcat
+    lua
+    luarocks
+    neovim
+    python
+    ripgrep
+    tlrc
+    yt-dlp
+    zoxide
 )
-
 
 # casks
 casks=(
-alt-tab
-anki
-calibre
-google-chrome
-iina
-karabiner-elements
-keycastr
-mactex
-macvim
-mks
-mpv
-obs
-pycharm-ce
-qbittorrent
-qq
-rectangle
-sioyek
-squirrel
-tex-live-utility
-the-unarchiver
-wechat
-wpsoffice-cn
+    alt-tab
+    anki
+    calibre
+    iina
+    karabiner-elements
+    keycastr
+    vimr
+    mks
+    stolendata-mpv
+    obs
+    qbittorrent
+    qq
+    rectangle
+    sioyek
+    squirrel
+    the-unarchiver
+    wechat
 )
 
-for formula in $formulae
-do
-		install formula $formula
+for formula in $formulae; do
+    install formula $formula
 done
 
-for cask in $casks
-do
-		install cask $cask
+for cask in $casks; do
+    install cask $cask
 done
-
